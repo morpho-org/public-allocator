98d3064595ca427b9b11f547dac4992643cdadf4
Adrien Husson <adhusson@gmail.com>
2024-02-14 03:04:45 +0100

    fmt: forge fmt

--- src/libraries/UtilsLib.sol
+++ src/libraries/UtilsLib.sol
@@ -12,16 +12,15 @@ library UtilsLib {
     /// @dev Returns `x` safely cast to uint128.
     function toUint128(uint256 x) internal pure returns (uint128) {
         if (x > type(uint128).max) {
-          revert ErrorsLib.MaxUint128Exceeded();
+            revert ErrorsLib.MaxUint128Exceeded();
         }
         return uint128(x);
     }
 
     // Returns min(x+y,type(uint128).max)
-    function saturatingAdd(uint128 x, uint128 y) internal pure returns (uint128 z)
-    {
+    function saturatingAdd(uint128 x, uint128 y) internal pure returns (uint128 z) {
         assembly ("memory-safe") {
-            let sum := add(x,y)
+            let sum := add(x, y)
             let ceil := 0x00000000000000000000000000000000ffffffffffffffffffffffffffffffff
             z := xor(sum, mul(xor(sum, ceil), lt(ceil, sum)))
         }
