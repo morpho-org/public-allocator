e2921a94bce643be61fbcd307c4e07d3bf1b8421
MathisGD <74971347+MathisGD@users.noreply.github.com>
2024-02-16 17:23:05 +0100

    refactor: remove useless errors
    Co-authored-by: Merlin Egalite <44097430+MerlinEgalite@users.noreply.github.com>
    Signed-off-by: MathisGD <74971347+MathisGD@users.noreply.github.com>

--- src/libraries/ErrorsLib.sol
+++ src/libraries/ErrorsLib.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.0;
 
-import {Id, MarketParams} from "../../lib/metamorpho/src/interfaces/IMetaMorpho.sol";
-import {Withdrawal} from "../interfaces/IPublicAllocator.sol";
+import {Id} from "../../lib/metamorpho/src/interfaces/IMetaMorpho.sol";
 
 /// @title ErrorsLib
 /// @author Morpho Labs
@@ -15,23 +14,11 @@ library ErrorsLib {
     /// @notice Thrown when the address passed is the zero address.
     error ZeroAddress();
 
-    /// @notice Thrown when the allocation to market `id` exceeds current max inflow.
-    error InflowCapExceeded(Id id);
-
-    /// @notice Thrown when the allocation from market `id` exceeds current max outflow.
-    error OutflowCapExceeded(Id id);
-
-    /// @notice Thrown when flow configuration for market `id` has min flow > max flow.
-    error InconsistentFlowConfig(Id id);
-
     /// @notice Thrown when the reallocation fee given is wrong.
     error IncorrectFee();
 
     /// @notice Thrown when the supply cap has been exceeded on market `id` during a reallocation of funds.
     error PublicAllocatorSupplyCapExceeded(Id id);
-
-    /// @notice Thrown when the maximum uint128 is exceeded.
-    error MaxUint128Exceeded();
 
     /// @notice Thrown when the value is already set.
     error AlreadySet();
